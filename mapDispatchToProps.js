1b313ef1fcb0837c83bb3a14e5298700eb386e13
atita <anaguasoft@gmail.com>
2018-07-07 06:03:22 +0300

    Map dispatch to props

---
+++ mapDispatchToProps.js
@@ -0,0 +1,10 @@
+const addMessage = (message) => {
+  return {
+    type: 'ADD',
+    message: message
+  }
+};
+// change code below this line
+const mapDispatchToProps = (dispatch) => ({
+   submitNewMessage: message => dispatch(addMessage(message))
+})
